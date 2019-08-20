06be5fb454204435343502b12aa355a9433bcc0d
Selin Huang <selin.hyx@gmail.com>
2019-08-19 23:13:24 -0400

    added employees

--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -9,3 +9,20 @@ puts "Exercise 6"
 puts "----------"
 
 # Your code goes here ...
+
+@store1 = Store.find(1)
+@store2 = Store.find(2)
+@store4 = Store.find(4)
+@store5 = Store.find(5)
+
+@store1.employees.create([
+  {first_name: "Khurram", last_name: "Virani", hourly_rate: 60},
+  {first_name: "Totoro", last_name: "T", hourly_rate: 100}
+])
+
+@store2.employees.create(first_name: "Toto", last_name: "Vir", hourly_rate: 60)
+
+@store4.employees.create(first_name: "Tot4", last_name: "Vi4", hourly_rate: 60)
+@store5.employees.create(first_name: "Totoro5", last_name: "Virani5", hourly_rate: 60)
+
+
