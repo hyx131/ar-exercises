1d8b95648e0cd21b3dc2a8009c1b7a954631a234
Selin Huang <selin.hyx@gmail.com>
2019-08-19 22:33:00 -0400

    selected stores based on conditions

--- exercises/exercise_4.rb
+++ exercises/exercise_4.rb
@@ -7,3 +7,22 @@ puts "Exercise 4"
 puts "----------"
 
 # Your code goes here ...
+
+Surrey = Store.create(name: "Surrey", annual_revenue: 224000, mens_apparel: false, womens_apparel: false)
+Whistler = Store.create(name: "Whistler", annual_revenue: 1900000, mens_apparel: true, womens_apparel: false)
+Yaletown = Store.create(name: "Yaletown", annual_revenue: 430000, mens_apparel: true, womens_apparel: true)
+puts Store.count
+
+@mens_stores = Store.where(mens_apparel: true)
+
+@mens_stores.each {|store| puts "store: #{store.name}, anual_revenue: #{store.annual_revenue}"}
+
+@womens_stores = Store.where(womens_apparel: true).where('annual_revenue <= 1000000')
+
+@womens_stores.each {|store| puts "store: #{store.name}, anual_revenue: #{store.annual_revenue}"}
+
+
+
+# Surrey (annual_revenue of 224000, carries women's apparel only)
+# Whistler (annual_revenue of 1900000 carries men's apparel only)
+# Yaletown (annual_revenue of 430000 carries men's and women's apparel)
