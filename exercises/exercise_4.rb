1e3f40030b21fa375c4a7a6fb6c0ef4d1319f265
Selin Huang <selin.hyx@gmail.com>
2019-08-19 22:46:23 -0400

    sum and average

--- exercises/exercise_4.rb
+++ exercises/exercise_4.rb
@@ -8,7 +8,7 @@ puts "----------"
 
 # Your code goes here ...
 
-Surrey = Store.create(name: "Surrey", annual_revenue: 224000, mens_apparel: false, womens_apparel: false)
+Surrey = Store.create(name: "Surrey", annual_revenue: 224000, mens_apparel: false, womens_apparel: true)
 Whistler = Store.create(name: "Whistler", annual_revenue: 1900000, mens_apparel: true, womens_apparel: false)
 Yaletown = Store.create(name: "Yaletown", annual_revenue: 430000, mens_apparel: true, womens_apparel: true)
 puts Store.count
