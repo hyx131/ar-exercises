965e2f8b6b98b4593c5fb2a65d32776ec40c8d24
Selin Huang <selin.hyx@gmail.com>
2019-08-19 22:19:26 -0400

    destroyed table 3

--- setup.rb
+++ setup.rb
@@ -10,8 +10,8 @@ puts 'Establishing connection to database ...'
 ActiveRecord::Base.establish_connection(
   adapter: 'postgresql',
   database: 'ar_exercises',
-  username: 'development',
-  password: 'development',
+  username: 'Yongxin',
+  password: 'Yongxin',
   host: 'localhost',
   port: 5432,
   pool: 5,
