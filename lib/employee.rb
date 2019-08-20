c1addc2ec0ec3fd0e6c5597a925678c418cab5a0
Selin Huang <selin.hyx@gmail.com>
2019-08-20 11:37:38 -0400

    created password with after_create callbalck

--- lib/employee.rb
+++ lib/employee.rb
@@ -5,9 +5,19 @@ class Employee < ActiveRecord::Base
   validates :hourly_rate, presence: true, numericality: { greater_than_or_equal_to: 40, less_than_or_equal_to: 200 }
   validates :store_id, presence: true, allow_nil: false, allow_blank: false
 
-  before_create do |employee|
-    o = [('a'..'z'), ('A'..'Z')].map(&:to_a).flatten
-    string = (0...8).map { o[rand(o.length)] }.join
-    employee.password = string
+  # before_create do |employee|
+  #   employee.password = generate_string
+  # end
+
+  after_create do |employee|
+    Employee.find(employee.id).update(password: generate_string)
   end
+
+  private
+
+  def generate_string
+    o = [('a'..'z'), ('A'..'Z')].map(&:to_a).flatten
+    (0...8).map { o[rand(o.length)] }.join
+  end
+
 end
